ed22e85b96c8d62e05629676afd201f3066efac7
Lilan Yang <bel@Lilans-MacBook-Pro.local>
2020-10-30 14:37:52 -0400

    sensor needed for instrument

--- exercise-3/exercise-3.ino
+++ exercise-3/exercise-3.ino
@@ -12,17 +12,17 @@ Teensy LC starter code
 */
 
 // analog values array size, must be constant
-const int num_of_analog_pins = 0; // total numer of analog pins
+const int num_of_analog_pins = 4; // total numer of analog pins
 int analog_values[num_of_analog_pins];
 int analog_pins[] = {
-  // list each analog pin to use
+  23, 16, 15, 14// list each analog pin to use
 };
 
 // digital_values array size, must be constant
-const int num_of_digital_pins = 5; // total numer of digital pins
+const int num_of_digital_pins = 3; // total numer of digital pins
 int digital_values[num_of_digital_pins];
 int digital_pins [] = {
-  14, 15, 16, 17, 23 // list each digital pin to use
+  9, 10, 11 // list each digital pin to use
 };
 
 
@@ -31,18 +31,18 @@ void setup() {
  // analogReadResolution(12); // Teensy is 12bit ADC, you can set to 12 but this will introduce some noise, also you can set lower like 7bit for MIDI range
   analogReadAveraging(32); // Teensy can do averaging of samples in hardware, set to 4, 8, 16, 32
 
-   pinMode(14, INPUT_PULLUP);  // set pin modes individually
-    pinMode(15, INPUT_PULLUP);
-    pinMode(16, INPUT_PULLUP);
-    pinMode(17, INPUT_PULLUP);
-    pinMode(23, INPUT_PULLUP);
-  // pinMode(5, INPUT_PULLUP);
+  //  pinMode(0, INPUT_PULLUP);  // set pin modes individually
+  //  pinMode(15, INPUT_PULLUP);
+  //  pinMode(16, INPUT_PULLUP);
+  //  pinMode(17, INPUT_PULLUP);
+  //  pinMode(23, INPUT_PULLUP);
+  //  pinMode(5, INPUT_PULLUP);
   //  pinMode(6, INPUT_PULLUP);
   //  pinMode(7, INPUT_PULLUP);
   //  pinMode(8, INPUT_PULLUP);
-  //  pinMode(9, INPUT_PULLUP);
-  //  pinMode(10, INPUT_PULLUP);
-  //  pinMode(11, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+    pinMode(10, INPUT_PULLUP);
+    pinMode(11, INPUT_PULLUP);
   //  pinMode(12, INPUT_PULLUP);
   //  pinMode(13, INPUT_PULLUP);
 
